5e644bb5473bd1a48207d40bb1d96516e8a62ba3
Rakshan <shetty.raxx555@gmail.com>
2018-07-11 12:37:29 +0530

    YCM fix

--- install/vim.sh
+++ install/vim.sh
@@ -1,6 +1,7 @@
 ln -s ~/dotfiles/vim $HOME/.vim
 vim +PluginInstall +qall
 
+sudo apt-get -y install cmake
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 ./install.sh --js-completer
