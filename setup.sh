4963a3aaba72c8bcc660ce91cdce1eb294c51e4e
Rakshan Shetty <shetty.raxx555@gmail.com>
2019-10-28 21:39:03 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -273,8 +273,6 @@ cd ~/.local/share/fonts && curl -fLo "FuraMonoMono.otf" https://github.com/ryano
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-export FZF_DEFAULT_COMMAND='fd --type f'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 ###############################################################################
 # Caps to Esc                                                                 #
 ###############################################################################
