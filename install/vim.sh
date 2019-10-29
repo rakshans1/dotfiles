0cfa14ab0b0266bd7672e31ff4d7d99cb0811ed3
Rakshan Shetty <shetty.raxx555@gmail.com>
2019-10-29 11:22:31 +0530

    Update

--- install/vim.sh
+++ install/vim.sh
@@ -1,8 +1,9 @@
 ln -fs ~/dotfiles/vim $HOME/.vim
 vim +PluginInstall +qall
 
-# sudo apt-get -y install cmake
-# cd ~/.vim/bundle/YouCompleteMe
-# git submodule update --init --recursive
-# ./install.sh --js-completer
+sudo apt-get -y install cmake
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+python3 install.py --js-completer
+
 
