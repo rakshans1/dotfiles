8924b75d59fb2e26ae8af2b2c5af82cc36a8142c
Rakshan Shetty <shetty.raxx555@gmail.com>
2019-11-18 21:56:13 +0530

    Symlink updated

--- install/vim.sh
+++ install/vim.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 
-echo -e "\\n\\nCreating vim symlinks"
-VIMFILES=( "$HOME/.vim:~/dotfiles/config/nvim"
-        "$HOME/.vimrc:~/dotfiles/config/nvim/init.vim" )
-
-for file in "${VIMFILES[@]}"; do
-    KEY=${file%%:*}
-    VALUE=${file#*:}
-    if [ -e "${KEY}" ]; then
-        echo "${KEY} already exists... skipping."
-    else
-        echo "Creating symlink for $KEY"
-        echo $VALUE
-        ln -s "${VALUE}" "${KEY}"
-    fi
-done
+ln -sf ~/dotfiles/config/nvim/init.vim ~/.vimrc
+ln -sf ~/dotfiles/config/nvim ~/.vim
