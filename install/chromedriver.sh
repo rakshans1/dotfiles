7ff1a6c9bf0d1303ef632bcafe7ed8aebe690276
Rakshan Shetty <shetty.raxx555@gmail.com>
2021-01-20 22:22:40 +0530

    update

--- install/chromedriver.sh
+++ install/chromedriver.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
-
+set -e
 # Chrome Driver
+
+OS="$(uname -s)"
+
+case $OS in
+    "Linux")
+        PLATFORM="linux64"
+    ;;
+    "Darwin")
+        PLATFORM="mac64"
+    ;;
+esac
+
 BASE_URL=https://chromedriver.storage.googleapis.com
 VERSION=$(curl -sL "$BASE_URL/LATEST_RELEASE")
-curl -sL "$BASE_URL/$VERSION/chromedriver_linux64.zip" -o  /tmp/driver.zip
-unzip /tmp/driver.zip
-chmod 755 chromedriver
-sudo mv chromedriver /usr/bin/chromedriver
+TEMP_DIRECTORY=$(mktemp -d)
+
+echo "Downloading $PACKAGE_NAME ..."
+wget -q --show-progress "$BASE_URL/$VERSION/chromedriver_$PLATFORM.zip" -O  "$TEMP_DIRECTORY/chromedriver.zip"
+
+if [ $? -ne 0 ]; then
+    echo "Download failed! Exiting."
+    exit 1
+fi
+
+echo "Extracting File..."
+unzip "$TEMP_DIRECTORY/chromedriver.zip" -d $TEMP_DIRECTORY/
+sudo mv "$TEMP_DIRECTORY/chromedriver" /usr/local/bin/
---
+++ install/tpm.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+~/.tmux/plugins/tpm/bin/install_plugins
