52d69bea1b91bbfe8d0d5dd817c9e66cba2a7b51
Rakshan Shetty <shetty.raxx555@gmail.com>
2025-08-30 19:39:25 +0530

    Fix AWS module - remove Nix interpolations from comments

--- nixpkgs/home-manager/modules/aws.nix
+++ nixpkgs/home-manager/modules/aws.nix
@@ -19,7 +19,7 @@
     # output = json
   '';
 
-  # Use activation script to create credentials file with actual secret values
+    # Use activation script to create credentials file with actual secret values
   home.activation.awsCredentials = lib.hm.dag.entryAfter ["writeBoundary"] ''
     $DRY_RUN_CMD mkdir -p $HOME/.aws
     $DRY_RUN_CMD chmod 700 $HOME/.aws
@@ -33,8 +33,8 @@ aws_secret_access_key = $(cat ${config.sops.secrets.aws_secret_access_key.path})
 
 # Add additional credential profiles here as needed:
 # [dev]
-# aws_access_key_id = \$(cat \${config.sops.secrets.aws_dev_access_key_id.path})
-# aws_secret_access_key = \$(cat \${config.sops.secrets.aws_dev_secret_access_key.path})
+# aws_access_key_id = REPLACE_WITH_DEV_KEY_ID
+# aws_secret_access_key = REPLACE_WITH_DEV_SECRET_KEY
 EOF
 
       $DRY_RUN_CMD chmod 600 $HOME/.aws/credentials
